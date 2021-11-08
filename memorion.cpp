b5ffe4c3cf8d8f15ba057ce6c593f8db543112ca
Poliest <93813028+Poliest@users.noreply.github.com>
2021-11-08 12:11:47 +0100

    Update memorion.cpp

--- memorion.cpp
+++ memorion.cpp
@@ -1,1 +1,45 @@
-few
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int tablesize;
+    char columna ='A';
+    int fila = 1;
+    string celda ="   |";
+    string salto ="----";
+
+    cout<<"INTRODUCE TAMAÑO DEL TABLERO = ";
+    cin >> tablesize;
+
+/*Tablero*/
+    cout<<celda;
+        for(int j=1; j<=tablesize; j++){
+            cout<<" "<<columna<<" |";
+            columna++;
+        }
+        cout<<endl;
+        for(int j=1;j<=tablesize+1; j++){
+            cout<<salto;
+        }
+        cout<<endl;
+    for(int i=1; i<=tablesize; i++){
+        cout<<" "<<fila<<" |";
+        fila++;
+        for(int j=1; j<tablesize+1; j++){
+            cout<<celda;
+        }
+        cout<<endl;
+        for(int j=1;j<=tablesize+1; j++){
+            cout<<salto;
+        }
+        cout<<endl;
+    }
+    char matriz[4][4];
+    matriz[1][1]= 'x';
+    cout<< matriz[1][1];
+
+
+ 
+}
