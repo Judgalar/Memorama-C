d0507d84e3075d2f49e214ed31eb2c3e3e67d7a1
Poliest <93813028+Poliest@users.noreply.github.com>
2021-11-12 14:33:35 +0100

    Update memorion.cpp

--- memorion.cpp
+++ memorion.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 #include <string>
+#include<stdlib.h>
+#include<time.h>
 using namespace std;
 
 int main()
 {
+    srand(time(NULL));
+    int num;
+    int casillaRandom;
     int tablesize;
-    char columna ='A';
+    int filaJugador1, columnaJugador1,filaJugador2, columnaJugador2;
+    int columna =1;
     int fila = 1;
+    char letra='A';
     string celda ="   |";
     string salto ="----";
 
     cout<<"INTRODUCE TAMAÑO DEL TABLERO = ";
     cin >> tablesize;
-
-/*Tablero*/
+    num = rand()%(tablesize*tablesize+1)/2; // numero random//
+    
+/*Tablero inicial*/
     cout<<celda;
         for(int j=1; j<=tablesize; j++){
             cout<<" "<<columna<<" |";
@@ -36,10 +44,24 @@ int main()
         }
         cout<<endl;
     }
-    char matriz[4][4];
-    matriz[1][1]= 'x';
-    cout<< matriz[1][1];
 
+    int matriz[tablesize][tablesize];
+    for(int i=0; i<tablesize ;i++){
+        matriz[i][i]= 
+    }
+
+    cout<<"Introduce fila";
+    cin>>filaJugador1;
+    cout<<"Introduce columna";
+    cin>>columnaJugador1;
+    cout<<matriz[filaJugador1][columnaJugador1];
+    cout<<"Introduce fila";
+    cin>>filaJugador2;
+    cout<<"Introduce columna";
+    cin>>columnaJugador2;
+    cout<<matriz[filaJugador2][columnaJugador2];
+    
+    if(matriz[filaJugador1][columnaJugador1]==matriz[filaJugador2][columnaJugador2]) cout<<"has acertado";
 
  
 }
