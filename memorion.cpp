6fef874bcd7c1b989586586aaa466daf77cc9687
Poliest <93813028+Poliest@users.noreply.github.com>
2021-11-15 05:51:11 +0100

    Update memorion.cpp

--- memorion.cpp
+++ memorion.cpp
@@ -8,6 +8,7 @@ int main()
 {
     srand(time(NULL));
     int num;
+    int num2;
     int casillaRandom;
     int tablesize;
     int filaJugador1, columnaJugador1,filaJugador2, columnaJugador2;
@@ -17,11 +18,15 @@ int main()
     string celda ="   |";
     string salto ="----";
 
-    cout<<"INTRODUCE TAMAÑO DEL TABLERO = ";
+    cout<<"INTRODUCE TAMAÑO DEL TABLERO (x*x=par) = ";
     cin >> tablesize;
-    num = rand()%(tablesize*tablesize+1)/2; // numero random//
+    while( (tablesize*tablesize)%2 != 0 ){
+     cout<<"INTRODUCE TAMAÑO DEL TABLERO (x*x=par) = ";
+     cin>>tablesize; 
+    }
+   
     
-/*Tablero inicial*/
+//Tablero inicial
     cout<<celda;
         for(int j=1; j<=tablesize; j++){
             cout<<" "<<columna<<" |";
@@ -46,22 +51,34 @@ int main()
     }
 
     int matriz[tablesize][tablesize];
-    for(int i=0; i<tablesize ;i++){
-        matriz[i][i]= 
+    char parejasAleatorias[tablesize*tablesize];
+    bool cadena[tablesize*tablesize]; for(int i=0; i<(tablesize*tablesize); i++) cadena[i]=0; // cadena booleana con todos los números establecidos a 0
+
+    for(int i=0; i<(tablesize*tablesize)/2; i++){ //HACER UNA FUNCION
+        num=rand() % (tablesize*tablesize);
+        num2=rand() % (tablesize*tablesize);
+        parejasAleatorias[num]=letra; cadena[num]=1;
+        while(num==num2) num2=rand() % (tablesize*tablesize);
+        parejasAleatorias[num2]=letra; cadena[num2]=1;
     }
 
-    cout<<"Introduce fila";
+    cout<<"Introduce fila = ";
     cin>>filaJugador1;
-    cout<<"Introduce columna";
+    cout<<"Introduce columna = ";
     cin>>columnaJugador1;
     cout<<matriz[filaJugador1][columnaJugador1];
-    cout<<"Introduce fila";
+    cout<<"Introduce fila = ";
     cin>>filaJugador2;
-    cout<<"Introduce columna";
+    cout<<"Introduce columna = ";
     cin>>columnaJugador2;
     cout<<matriz[filaJugador2][columnaJugador2];
     
     if(matriz[filaJugador1][columnaJugador1]==matriz[filaJugador2][columnaJugador2]) cout<<"has acertado";
 
  
+
+ // FIN DEL PROGRAMA
+ cout<<endl<<endl;
+ system("pause");
+ return 0;
 }
